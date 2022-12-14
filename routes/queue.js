a1ec3f2b91b1e28a758a00406987cd4b49abce4d
kittinan <kittinan.boat@gmail.com>
2022-12-14 23:41:58 +0700

    1.4

--- routes/queue.js
+++ routes/queue.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Router = express.Router();
-
+const moment = require("moment");
+const ObjectID = require("mongodb").ObjectID;
 let Queue = require("../schema/queue");
 
 Router.get("/", (req, res, next) => {
@@ -19,6 +20,82 @@ Router.get("/", (req, res, next) => {
     });
 });
 
+Router.get("/day", (req, res, next) => {
+  const { doctorId } = req.query;
+  var startDate = moment().startOf("day").toDate();
+  var endDate = moment().endOf("day").toDate();
+  const con = {
+    $match: {
+      startDate: {
+        $gte: new Date(startDate),
+        $lte: new Date(endDate),
+      },
+    },
+  };
+  if (doctorId && doctorId != "null") {
+    con["$match"]["doctorId"] = new ObjectID(doctorId);
+  }
+  Queue.aggregate([con])
+    .sort({ startDate: 1 })
+    .exec((err, result) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(result);
+      }
+    });
+});
+Router.get("/week", (req, res, next) => {
+  const { doctorId } = req.query;
+  var startDate = moment().startOf("week").add(1, "day").toDate();
+  var endDate = moment().endOf("week").add(1, "day").toDate();
+  const con = {
+    $match: {
+      startDate: {
+        $gte: new Date(startDate),
+        $lte: new Date(endDate),
+      },
+    },
+  };
+  if (doctorId && doctorId != "null") {
+    con["$match"]["doctorId"] = new ObjectID(doctorId);
+  }
+  Queue.aggregate([con])
+    .sort({ startDate: 1 })
+    .exec((err, result) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(result);
+      }
+    });
+});
+Router.get("/month", (req, res, next) => {
+  const { doctorId } = req.query;
+  var startDate = moment().startOf("month").toDate();
+  var endDate = moment().endOf("month").toDate();
+  const con = {
+    $match: {
+      startDate: {
+        $gte: new Date(startDate),
+        $lte: new Date(endDate),
+      },
+    },
+  };
+  if (doctorId && doctorId != "null") {
+    con["$match"]["doctorId"] = new ObjectID(doctorId);
+  }
+  Queue.aggregate([con])
+    .sort({ startDate: 1 })
+    .exec((err, result) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(result);
+      }
+    });
+});
+
 Router.post("/add", (req, res, next) => {
   Queue.insertMany(req.body, (err, rs) => {
     if (err) {
@@ -28,7 +105,6 @@ Router.post("/add", (req, res, next) => {
     }
   });
 });
-
 
 Router.put("/update/:id", (req, res, next) => {
   const { id } = req.params;
