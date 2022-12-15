e2f27b7e49a9444611dc1ac6d45231ae127a9c51
kittinan <kittinan.boat@gmail.com>
2022-12-16 00:05:08 +0700

    1.5

--- routes/queue.js
+++ routes/queue.js
@@ -95,15 +95,58 @@ Router.get("/month", (req, res, next) => {
       }
     });
 });
+Router.get("/period", (req, res, next) => {
+  let { doctorId, start, end } = req.query;
+  if (start == end) {
+    end = moment(end).endOf("day");
+  }
+  var startDate = moment(start).toDate();
+  var endDate = moment(end).toDate();
+  const con = {
+    $match: {
+      startDate: {
+        $gte: new Date(startDate),
+        $lte: new Date(endDate),
+      },
+    },
+  };
+  if (doctorId && doctorId != "null") {
+    con["$match"]["doctorId"] = new ObjectID(doctorId);
+  }
+  Queue.aggregate([con])
+    .sort({ startDate: 1 })
+    .exec((err, result) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(result);
+      }
+    });
+});
 
-Router.post("/add", (req, res, next) => {
-  Queue.insertMany(req.body, (err, rs) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(rs);
-    }
-  });
+Router.post("/add", async (req, res, next) => {
+  console.log(req.body);
+  const foo = await Queue.aggregate([
+    {
+      $match: {
+        doctorId: new ObjectID(req.body.doctorId),
+        startDate: {
+          $eq: new Date(req.body.startDate),
+        },
+      },
+    },
+  ]);
+  if (foo && foo.length > 0) {
+    res.json({ error: true, data: foo });
+  } else {
+    Queue.insertMany(req.body, (err, rs) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(rs);
+      }
+    });
+  }
 });
 
 Router.put("/update/:id", (req, res, next) => {
